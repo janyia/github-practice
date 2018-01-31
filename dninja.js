d289e78bf53c4e1edbb045a74195af5ecb78a0ac
Rich <rjanyia@yahoo.com>
2018-01-31 13:11:48 -0600

    firsi attempt

---
+++ dninja.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    console.log('document loaded')
+    $('#img1').click(function(){
+        $('#img1').css('opacity',"0");     
+    })
+    $('#img2').click(function(){
+        $('#img2').css('opacity',"0");
+    })
+    $('#img3').click(function(){
+        $('#img3').css('opacity',"0");
+    })
+    $('#img4').click(function(){
+        $('#img4').css('opacity',"0");
+    })
+    $('#img5').click(function(){
+        $('#img5').css('opacity',"0");
+    })
+    $('#img6').click(function(){
+        $('#img6').css('opacity',"0");
+    })
+    $('#img7').click(function(){
+        $('#img7').css('opacity',"0");
+    })
+    $('#img8').click(function(){
+        $('#img8').css('opacity',"0");
+    })
+    $('button').click(function(){
+       $('img').css('opacity', '100');
+       
+    })
+})
